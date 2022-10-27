a5dbd584382fb28658dddacd2efe698de0d266ec
Jackson <jviana116@gmail.com>
2022-10-27 19:47:54 -0300

    fix: data test id

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -4,38 +4,95 @@ function Checkout() {
   return (
     <div>
       <header>
-        <div data-testid="11">Produtos</div>
-        <div data-testid="12">Meus Pedidos</div>
-        <div data-testid="13">Nome do Usuário</div>
-        <button type="button" data-testid="14">Sair</button>
-      </header>
-      <section>
-        <h3>Finalizar Pedido</h3>
-        <div>
-          <span>Item</span>
-          <span>Descrição</span>
-          <span>Quantidade</span>
-          <span>Valor Unitário</span>
-          <span>Sub-total</span>
-          <button type="button">Remover Item</button>
-          <h1 data-testid="28">Total</h1>
+        <div data-testid="customer_products__element-navbar-link-products">Produtos</div>
+        <div
+          data-testid="customer_products__element-navbar-link-orders"
+        >
+          Meus Pedidos
         </div>
+        <div
+          data-testid="customer_products__element-navbar-user-full-name"
+        >
+          Nome do Usuário
+        </div>
+        <button
+          type="button"
+          data-testid="customer_products__element-navbar-link-logout"
+        >
+          Sair
+        </button>
+      </header>
+      <h3>Finalizar Pedido</h3>
+      <table>
+        <tr>
+          <th>Item</th>
+          <th>Descrição</th>
+          <th>Quantidade</th>
+          <th>Valor Unitário</th>
+          <th>Sub-total</th>
+          <th>Remover Item</th>
+        </tr>
+        <tr>
+          <td
+            data-testid="customer_checkout__element-order-table-item-number-1"
+          >
+            1
+          </td>
+          <td
+            data-testid="customer_checkout__element-order-table-name-1"
+          >
+            Camisa
+          </td>
+          <td
+            data-testid="customer_checkout__element-order-table-quantity-1"
+          >
+            1
+          </td>
+          <td
+            data-testid="customer_checkout__element-order-table-unit-price-1"
+          >
+            R$ 50,00
+
+          </td>
+          <td
+            data-testid="customer_checkout__element-order-table-sub-total-1"
+          >
+            R$ 50,00
+
+          </td>
+          <td>
+            <button
+              type="button"
+              data-testid="customer_checkout__element-order-table-remove-1"
+            >
+              Remover
+            </button>
+          </td>
+        </tr>
+      </table>
+      <section>
+        <h1 data-testid="customer_checkout__element-order-total-price">Total</h1>
         <h3>Detalhes e Endereço para Entrega</h3>
         <div>
           <span>P.Vendedora Responsável</span>
-          <select data-testid="29">
+          <select data-testid="customer_checkout__select-seller">
             <option value="option1">Vendedora 1</option>
           </select>
         </div>
         <div>
           <span>Endereço</span>
-          <input type="text" data-testid="30" />
+          <input type="text" data-testid="customer_checkout__input-address" />
         </div>
         <div>
           <span>Número</span>
-          <input type="number" data-testid="31" />
+          <input type="number" data-testid="customer_checkout__input-address-number" />
         </div>
-        <button type="button" data-testid="32">Finalizar Pedido</button>
+        <button
+          type="button"
+          data-testid="customer_checkout__button-submit-order"
+        >
+          Finalizar Pedido
+        </button>
       </section>
     </div>
   );
