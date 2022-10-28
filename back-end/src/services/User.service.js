17335e3cb5372db2b6a71a934dee56fa9110a807
Matheus Monteiro <matheuschran@hotmail.com>
2022-10-28 13:53:43 -0300

    [backend feature] implementa uso de hash para senha na criação de usuásrios

--- back-end/src/services/User.service.js
+++ back-end/src/services/User.service.js
@@ -1,8 +1,10 @@
 const { User } = require('../database/models');
+const md5 = require('md5');
 
 const createCustomerUser = async (newUser) => {
   const user = await User.create({
     ...newUser,
+    password: md5(newUser.password),
     role: 'customer',
   });
   return user;
