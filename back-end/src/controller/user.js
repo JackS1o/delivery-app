54dbbbc4a3fc9099d43145c68d1c683d632b7ff5
Gisele <gisele.viedenhelfen@gmail.com>
2022-10-31 19:24:30 -0300

    corrige lint

--- back-end/src/controller/user.js
+++ back-end/src/controller/user.js
@@ -1,8 +1,16 @@
 const userService = require('../service/user');
 
 const getAll = async (req, res) => {
-  await userService.getUser();
-  return res.status(200).json({ message: 'login efetuado' });
+  const { email, password } = req.body;
+  const getUser = await userService.getUser();
+  const filteredUser = getUser.filter((users) => users.email === email);
+  const dataUser = {
+    name: filteredUser[0].name,
+    email: filteredUser[0].email,
+    role: filteredUser[0].role,
+    password,
+  };
+  return res.status(200).json(dataUser);
  };
 
  module.exports = { getAll };
--- back-end/src/service/user.js
+++ back-end/src/service/user.js
@@ -1,7 +1,6 @@
 const { user } = require('../database/models');
 
 const getUser = async () => {
-  // console.log(typeof user);
   const userData = await user.findAll();
   const result = userData.map((item) => item.dataValues);
   return result;
