c16858180a4c59791aa4d452b5aae35f6d22e5ca
Matheus Monteiro <matheuschran@hotmail.com>
2022-11-01 16:07:56 -0300

    corrige erro no backend

--- back-end/src/database/models/sales.model.js
+++ back-end/src/database/models/sales.model.js
@@ -51,10 +51,6 @@ module.exports = (sequelize, DataTypes) => {
       { foreignKey: "user_id", as: "user" },
       { foreignKey: "seller_id", as: "seller" }
     );
-    Sale.hasMany(models.SalesProduct, {
-      foreignKey: "sale_id",
-      as: "salesProducts",
-    });
   };
   return Sale;
 };
