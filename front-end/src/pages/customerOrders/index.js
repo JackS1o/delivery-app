8823a39df304db1ce159ba89b5d51dc346e4c1c6
Gisele <gisele.viedenhelfen@gmail.com>
2022-11-02 14:37:48 -0300

    correção de qual pagina é a tarefa

--- front-end/src/pages/customerOrders/index.js
+++ front-end/src/pages/customerOrders/index.js
@@ -7,59 +7,26 @@ function CustumerOrder() {
       <Header />
       <section>
         <p
-          data-testid="customer_order_details__element-order-details-label-order-id"
+          data-testid="customer_orders__element-order-id-<id>"
         >
           pedido nº tal
         </p>
         <p
-          data-testid="customer_order_details__element-order-details-label-seller-name"
+          data-testid="customer_orders__element-delivery-status-<id>"
         >
-          vendedor: nome tal
+          status do pedido
         </p>
         <p
-          data-testid="customer_order_details__element-order-details-label-order-date"
+          data-testid="customer_orders__element-order-date-<id>"
         >
           data da venda: dd/mm/aa
         </p>
-        <button
-          type="button"
-          data-testid="customer_order_details__button-delivery-check"
-        >
-          marcar como entregue
-        </button>
-      </section>
-      <section>
         <p
-          data-testid="customer_order_details__element-order-table-item-number-<index>"
+          data-testid="customer_orders__element-card-price-<id>"
         >
-          1 (ordem do item no array de pedidos)
-        </p>
-        <p
-          data-testid="customer_order_details__element-order-table-name-<index>"
-        >
-          nome do item
-        </p>
-        <p
-          data-testid="customer_order_details__element-order-table-quantity-<index>"
-        >
-          quantidade
-        </p>
-        <p
-          data-testid="customer_order_details__element-order-table-unit-price-<index>"
-        >
-          valor unitário
-        </p>
-        <p
-          data-testid="customer_order_details__element-order-table-sub-total-<index>"
-        >
-          sub-total
+          preço do pedido
         </p>
       </section>
-      <p
-        data-testid="customer_order_details__element-order-total-price"
-      >
-        total: valor
-      </p>
     </div>
   );
 }
