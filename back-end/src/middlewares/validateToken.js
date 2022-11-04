2a066d63bb27b3c4441aa4626f330c2a16da0069
Jackson <jviana116@gmail.com>
2022-11-03 21:00:37 -0300

    fix: jwt token

--- back-end/src/middlewares/validateToken.js
+++ back-end/src/middlewares/validateToken.js
@@ -1,9 +1,11 @@
 const { jwt } = require('jsonwebtoken');
+const jwtKey = require('fs')
+  .readFileSync('jwt.evaluation.key', { encoding: 'utf-8' });
 
 const validateToken = async (req, res, next) => {
   const { authorization } = req.headers;
   try {
-    jwt.verify(authorization, process.env.JWT_SECRET);
+    jwt.verify(authorization, jwtKey);
   } catch (error) {
     return res.status(401).json({ message: 'Token inválido ou expirado' });
   }
