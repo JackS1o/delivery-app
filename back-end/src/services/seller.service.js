d6d7c35511819e0ecd376a4d6bd9e6e9d43b709e
Jackson <jviana116@gmail.com>
2022-11-09 13:04:59 -0300

    fix: lint

--- back-end/src/services/seller.service.js
+++ back-end/src/services/seller.service.js
@@ -4,38 +4,27 @@ const {
   salesProducts,
   sequelize,
   products,
-} = require("../database/models");
+} = require('../database/models');
 
 const getSeller = async () => {
-  const sellerData = await user.findAll({ where: { role: "seller" } });
+  const sellerData = await user.findAll({ where: { role: 'seller' } });
   return sellerData;
 };
 
 const createSale = async (body) => {
   const {
-    userId,
-    sellerId,
-    totalPrice,
-    deliveryAddress,
-    deliveryNumber,
-    status,
-    order,
+    userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status, order,
   } = body;
   const result = await sequelize.transaction(async (transaction) => {
     const saleData = await sale.create({
-      userId,
-      sellerId,
-      totalPrice,
-      deliveryAddress,
-      deliveryNumber,
-      status,
+      userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status,
     });
-    const products = await order.order.map((product) => ({
+    const prod = await order.order.map((product) => ({
       saleId: saleData.dataValues.id,
       productId: product.id,
       quantity: product.quantity,
     }));
-    await salesProducts.bulkCreate(products, { transaction });
+    await salesProducts.bulkCreate(prod, { transaction });
     return saleData;
   });
   return result;
@@ -66,8 +55,8 @@ const getSellerById = async (id) => {
     include: [
       {
         model: products,
-        as: "products",
-        through: { attributes: ["quantity"], as: "product" },
+        as: 'products',
+        through: { attributes: ['quantity'], as: 'product' },
       },
     ],
   });
